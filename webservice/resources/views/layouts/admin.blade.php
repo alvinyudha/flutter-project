c2293a7bc9b68f514c27dc91052b49db88055a80
Almaaslutfi <almaslutfi17@gmail.com>
2023-11-29 09:46:45 +0700

    update ui web

--- webservice/resources/views/layouts/admin.blade.php
+++ webservice/resources/views/layouts/admin.blade.php
@@ -14,7 +14,7 @@
     <meta property="og:image" content="http://pratikborsadiya.in/blog/vali-admin/hero-social.png">
     <meta property="og:description"
         content="Vali is a responsive and free admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.">
-    <title>@yield('title') | Sistem Kepegawaian</title>
+    <title>@yield('title') | Cuti APPS</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
